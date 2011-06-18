c62e59be698c47cd87ca17c930782cfc440c4c49
Simon Gate <smgt@buno.se>
2011-06-18 20:24:47 +0200

    Trying to validate the generated XML against schema

--- spec/epp_spec.rb
+++ spec/epp_spec.rb
@@ -1,7 +1,39 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require "nokogiri"
+require "markup_validity"
 
-describe "Epp" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+epp_schema = File.read(File.dirname(__FILE__) + "/schemas/all.xsd")
+
+describe "EPP::Builder" do
+  describe "login" do
+    it "should output valid xml" do
+      xml =  EPP::Builder.login("username", "password")
+      puts xml
+      xml.should be_valid_with_schema(epp_schema)
+    end
+  end
+end
+
+describe "EPP::Builder" do
+  describe "domain check" do
+    it "should output valid xml" do
+      fail
+    end
+  end
+end
+
+describe "EPP::Builder" do
+  describe "domain find" do
+    it "should output valid xml" do
+      fail
+    end
+  end
+end
+
+describe "EPP::Builder" do
+  describe "#domain_update" do
+    it "should output valid xml" do
+      fail
+    end
   end
 end
